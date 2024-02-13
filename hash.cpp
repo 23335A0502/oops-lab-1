9a2ed351c0e0fd84ee822eb86eb49e034d3a954f
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-02-13 19:45:21 +0530

    hash.cpp

---
+++ hash.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    float d=56.678;
+    string n;
+    cout<<"hi "<<endl;
+    cout<<"world"<<ends;
+    cout<<setw(10)<<"madhu"<<endl;
+    cout<<setw(10)<<setfill('*')<<"madhu"<<endl;
+    cout<<setprecision(11)<<d<<endl;
+    cout<<"the world"<<flush<<endl;
+    cout.flush();
+    cout<<"enter name : ";
+    cin>>ws;
+    cin>>n;
+    cout<<"the name is "<<n<<endl;
+}
+
