15afd60a7503f28151ff1d61decd76e254d6b445
23335A0502 <159770551+23335A0502@users.noreply.github.com>
2024-02-14 20:45:16 +0530

    hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,20 +1,9 @@
 #include<iostream>
-#include<iomanip>
 using namespace std;
 int main()
 {
-    float d=56.678;
-    string n;
-    cout<<"hi "<<endl;
-    cout<<"world"<<ends;
-    cout<<setw(10)<<"madhu"<<endl;
-    cout<<setw(10)<<setfill('*')<<"madhu"<<endl;
-    cout<<setprecision(11)<<d<<endl;
-    cout<<"the world"<<flush<<endl;
-    cout.flush();
-    cout<<"enter name : ";
-    cin>>ws;
-    cin>>n;
-    cout<<"the name is "<<n<<endl;
-}
-
+	cout<<"USING '\\n' ...\n";
+	cout<<"Line 1 \nLine 2 \nLine 3 \n";
+	cout<<"USING end ..."<< endl;
+	cout<<"Line 1"<<endl<<"Line 2"<<endl<<"Line 3"<<endl;
+} 
